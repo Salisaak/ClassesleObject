b611f13544f1f56f62c9baf7474be94e2ffa490c
Saahir <Salisaak@gmail.com>
2021-09-17 10:31:01 -0400

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,27 +1,24 @@
 package quickMafs;
 
 public class Calculator {
+    public double num1;
+    public double num2;
 
-    public Calculator(double num1, String operation, double num2){
-        if (operation == "+")
-        {
-            System.out.println("your answer is " + (num1 + num2));
+    public Calculator(double num1, double num2){
+            this.num1 = num1;
+            this.num2 = num2;
+
         }
-        else if  (operation == "-")
-        {
-            System.out.println("your answer is " + (num1 - num2));
+
+    public double addition(){return num1 + num2;}
+    public double multiplication(){return num1 * num2;}
+    public double subtraction(){return  num1 - num2;}
+    public double division(){return num1 /num2;}
+    public double by(){return (double) Math.pow(num1,num2);}
+
+
+
         }
-        else if (operation == "/")
-        {
-            System.out.println("your answer is " + (num1 / num2));
-        }
-        else if (operation == "*")
-        {
-            System.out.println("your answer is " + (num1 * num2));
-        }
-        else if (operation == "^")
-        {
-            System.out.println("your answer is " + (Math.pow(num1,num2)));
-        }
-    }
-}
+
+
+
