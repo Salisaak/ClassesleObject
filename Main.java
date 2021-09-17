235df44dfd30ddf1dd685081f279c251cadecf85
Saahir <Salisaak@gmail.com>
2021-09-17 11:02:34 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package quickMafs;
 public class Main {
 
 
+
     public static void main(String[] args) {
         //datatype--Name of variable = new datatype();
         Square jawn = new Square(15,20);
@@ -11,7 +12,7 @@ public class Main {
 
         Triangle illuminati = new Triangle(5, 4,7,9);
 
-        Calculator test = new Calculator(2,"^",3);
+        Calculator test = new Calculator(2,3);
 
         Dragon Escanor = new Dragon("Levania","Purple",120);
         Snake Avenarius = new Snake("Zach",20,"Extreme","Gold");
@@ -23,6 +24,13 @@ public class Main {
         System.out.println(Avenarius);
         System.out.println(emp1);
         System.out.println(emp2);
+        System.out.println(test.multiplication());
+        System.out.println(test.subtraction());
+        System.out.println(test.addition());
+        System.out.println(test.division());
+        System.out.println(test.multiplication());
+        System.out.println(test.by());
+
     }
 
 
